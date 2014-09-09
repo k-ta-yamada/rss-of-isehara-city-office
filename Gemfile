b6dcdb4a1dbc54ce2bb5382c125a695a8826b002
k-ta-yamada <key.luvless@gmail.com>
2014-09-09 18:05:16 +0900

    テンプレートエンジンをslimに、rssに詳細を追加

--- Gemfile
+++ Gemfile
@@ -4,9 +4,11 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem 'sinatra'
+gem 'slim'
 gem 'nokogiri'
 
 group :development do
   gem 'sinatra-contrib'
   gem 'pry'
+  gem 'byebug'
 end
--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'slim'
 require 'open-uri'
 require 'nokogiri'
 require 'rss'
@@ -6,6 +7,7 @@ require 'rss'
 if development?
   require 'sinatra/reloader'
   require 'pry'
+  require 'byebug'
 end
 
 TARGET_SITE = 'http://www.city.isehara.kanagawa.jp'
@@ -13,7 +15,7 @@ TARGET_URL = "#{TARGET_SITE}/topics.htm"
 THIS_SITE = 'http://rss-of-the-city-hall.herokuapp.com/'
 
 get '/' do
-  erb :index, locals: { nodes: ary_of_hash_for_rss(get_resource_node) }
+  slim :index, locals: { nodes: ary_of_hash_for_rss(contents_of_update_history_page) }
 end
 
 get '/rss' do
@@ -23,30 +25,35 @@ get '/rss' do
     maker.channel.description = '市役所公式HPの更新履歴をRSSにしたもの'
     maker.channel.link = THIS_SITE
 
-    ary_of_hash_for_rss(get_resource_node).each do |h|
+    ary_of_hash_for_rss(contents_of_update_history_page).each do |h|
       next if h[:link].nil?
       item = maker.items.new_item
       item.title = h[:title]
       item.link = h[:link]
+      item.description = h[:description]
     end
   end
 
   rss.to_s
 end
 
-def get_resource_node
-  html = open(TARGET_URL).read
-  doc = Nokogiri::HTML(html)
-  node = doc.css('.contentsArea li')
-  node
+def contents_of_update_history_page
+  doc = Nokogiri::HTML(open(TARGET_URL))
+  doc.css('.contentsArea li')
 end
 
-def ary_of_hash_for_rss(node)
+def contents_of_link_page(link)
+  doc = Nokogiri::HTML(open(link))
+  doc.css('.skip ~ *').map { |elm| elm.text }.join("\n")
+end
+
+def ary_of_hash_for_rss(nodes)
   result = []
-  node.each do |elm|
+  nodes.each do |elm|
     title = elm.text
     link = elm.css('a').first.nil? ? nil : "#{TARGET_SITE}#{elm.css('a').first['href']}"
-    result << { title: title, link: link }
+    description = link.nil? ? nil : contents_of_link_page(link)
+    result << { title: title, link: link, description: description }
   end
   result
 end
