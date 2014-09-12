f654ca218992b2e1684f48815f591a29a78ccade
k-ta-yamada <key.luvless@gmail.com>
2014-09-12 16:46:50 +0900

    指定時間以内の場合は情報の再取得をしない、rubocop対応、メソッドのクラス化

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'slim'
-require 'open-uri'
-require 'nokogiri'
-require 'rss'
+require './lib/rss_contents'
+
+# このサイト
+THIS_SITE = 'http://rss-of-the-city-hall.herokuapp.com'
 
 if development?
   require 'sinatra/reloader'
@@ -10,12 +11,10 @@ if development?
   require 'byebug'
 end
 
-TARGET_SITE = 'http://www.city.isehara.kanagawa.jp'
-TARGET_URL = "#{TARGET_SITE}/topics.htm"
-THIS_SITE = 'http://rss-of-the-city-hall.herokuapp.com/'
-
 get '/' do
-  slim :index, locals: { nodes: ary_of_hash_for_rss(contents_of_update_history_page) }
+  slim :index,
+       locals: { nodes: RssContents.data_for_rss,
+                 last_update: RssContents.instance_variable_get(:@last_update) }
 end
 
 get '/rss' do
@@ -25,7 +24,7 @@ get '/rss' do
     maker.channel.description = '市役所公式HPの更新履歴をRSSにしたもの'
     maker.channel.link = THIS_SITE
 
-    ary_of_hash_for_rss(contents_of_update_history_page).each do |h|
+    RssContents.data_for_rss.each do |h|
       next if h[:link].nil?
       item = maker.items.new_item
       item.title = h[:title]
@@ -35,25 +34,4 @@ get '/rss' do
   end
 
   rss.to_s
-end
-
-def contents_of_update_history_page
-  doc = Nokogiri::HTML(open(TARGET_URL))
-  doc.css('.contentsArea li')
-end
-
-def contents_of_link_page(link)
-  doc = Nokogiri::HTML(open(link))
-  doc.css('.skip ~ *').map { |elm| elm.text }.join("\n")
-end
-
-def ary_of_hash_for_rss(nodes)
-  result = []
-  nodes.each do |elm|
-    title = elm.text
-    link = elm.css('a').first.nil? ? nil : "#{TARGET_SITE}#{elm.css('a').first['href']}"
-    description = link.nil? ? nil : contents_of_link_page(link)
-    result << { title: title, link: link, description: description }
-  end
-  result
 end
---
+++ lib/rss_contents.rb
@@ -0,0 +1,75 @@
+# RSS用の情報取得などを行うクラス
+class RssContents
+  require 'open-uri'
+  require 'nokogiri'
+  require 'rss'
+
+  # 情報取得元サイト
+  TARGET_SITE = 'http://www.city.isehara.kanagawa.jp'
+  # 情報取得元サイトのURL
+  TARGET_URL = "#{TARGET_SITE}/topics.htm"
+  # 指定秒数以内かのチェックで使用する秒数を指定
+  SPECIFIED_TIME = 600
+
+  @result = []
+  @last_update = Time.now
+
+  class << self
+    # 取得情報をHashのArrayで返す
+    # @return [Array of Hash]
+    def data_for_rss
+      # 毎回TARGET_SITEを突かないようにするため
+      # 前回処理時間から指定時間以内かつ、@resultが空でない場合はreturn
+      return @result if within_specified_time? && !@result.empty?
+
+      @result = []
+      contents_of_update_history_page.each do |elm|
+        title = elm.text
+        link = elm.css('a').first.nil? ? nil : url_of_detail_contents(elm)
+        description = link.nil? ? nil : contents_of_detail_page(link)
+        @result << { title: title, link: link, description: description }
+      end
+      @last_update = Time.now
+      @result
+    end
+
+    # rubocop:disable Style/TrivialAccessors
+    # attr_readerへの変更を警告されるが、クラスインスタンス変数を使用したいため
+    def last_update_time
+      @last_update
+    end
+    # rubocop:enable Style/TrivialAccessors
+
+    private
+
+    # 前回実行時間から指定の秒数以内かどうかを返す
+    # @return
+    #   true  : 指定時間以内
+    #   false : 指定時間以上
+    def within_specified_time?
+      SPECIFIED_TIME > (Time.now - @last_update)
+    end
+
+    # 更新履歴ページから一覧を取得する
+    # @return [Array]
+    def contents_of_update_history_page
+      doc = Nokogiri::HTML(open(TARGET_URL))
+      doc.css('.contentsArea li')[0..1]
+    end
+
+    # 詳細ページのURLを生成する
+    # @param [Nokogiri::XML::Element] elm
+    # @return [String]
+    def url_of_detail_contents(elm)
+      "#{TARGET_SITE}#{elm.css('a').first['href']}"
+    end
+
+    # 更新履歴のリンク情報から詳細ページの情報を取得する
+    # @param [String] link 詳細ページのURL
+    # @return [String]
+    def contents_of_detail_page(link)
+      doc = Nokogiri::HTML(open(link))
+      doc.css('.skip ~ *').map { |elm| elm.text }.join("\n")
+    end
+  end
+end
