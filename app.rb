86e8d8a6ced71e604d5248fb75e29d021fb99615
k-ta-yamada <key.luvless@gmail.com>
2014-09-13 17:44:28 +0900

    RssContentsクラスの修正ほか軽微な修正

--- app.rb
+++ app.rb
@@ -14,21 +14,21 @@ end
 get '/' do
   slim :index,
        locals: { nodes: RssContents.data_for_rss,
-                 last_update: RssContents.instance_variable_get(:@last_update) }
+                 last_update: RssContents.last_update }
 end
 
 get '/rss' do
   rss = RSS::Maker.make('2.0') do |maker|
-    maker.channel.about = "#{THIS_SITE}/rss"
-    maker.channel.title = '市役所公式HPの更新履歴'
+    maker.channel.about       = "#{THIS_SITE}/rss"
+    maker.channel.title       = '市役所公式HPの更新履歴'
+    maker.channel.link        = THIS_SITE
     maker.channel.description = '市役所公式HPの更新履歴をRSSにしたもの'
-    maker.channel.link = THIS_SITE
 
     RssContents.data_for_rss.each do |h|
       next if h[:link].nil?
       item = maker.items.new_item
-      item.title = h[:title]
-      item.link = h[:link]
+      item.title       = h[:title]
+      item.link        = h[:link]
       item.description = h[:description]
     end
   end
--- lib/rss_contents.rb
+++ lib/rss_contents.rb
@@ -1,4 +1,3 @@
-# RSS用の情報取得などを行うクラス
 class RssContents
   require 'open-uri'
   require 'nokogiri'
@@ -16,39 +15,31 @@ class RssContents
 
   class << self
     # 取得情報をHashのArrayで返す
-    # @return [Array of Hash]
+    # @return [Array-of-Hash]
     def data_for_rss
       # 毎回TARGET_SITEを突かないようにするため
-      # 前回処理時間から指定時間以内かつ、@resultが空でない場合はreturn
-      return @result if within_specified_time? && !@result.empty?
+      return @result if use_result_of_previous?
 
       @result = []
-      contents_of_update_history_page.each do |elm|
-        title = elm.text
-        link = elm.css('a').first.nil? ? nil : url_of_detail_contents(elm)
-        description = link.nil? ? nil : contents_of_detail_page(link)
+      contents_of_update_history_page.each do |list_item|
+        title       = list_item.text
+        link        = link_to_detail_page(list_item)
+        description = contents_of_detail_page(link)
         @result << { title: title, link: link, description: description }
       end
       @last_update = Time.now
       @result
     end
 
-    # rubocop:disable Style/TrivialAccessors
-    # attr_readerへの変更を警告されるが、クラスインスタンス変数を使用したいため
-    def last_update_time
+    # クラスインスタンス変数の@last_updateを返す
+    # @return [Time]
+    # @note rubocopでattr_readerへの変更を警告されるが
+    #       クラスインスタンス変数を使用したいためにわざとやってる
+    def last_update   # rubocop:disable Style/TrivialAccessors
       @last_update
     end
-    # rubocop:enable Style/TrivialAccessors
 
     private
-
-    # 前回実行時間から指定の秒数以内かどうかを返す
-    # @return
-    #   true  : 指定時間以内
-    #   false : 指定時間以上
-    def within_specified_time?
-      SPECIFIED_TIME > (Time.now - @last_update)
-    end
 
     # 更新履歴ページから一覧を取得する
     # @return [Array]
@@ -57,19 +48,35 @@ class RssContents
       doc.css('.contentsArea li')
     end
 
-    # 詳細ページのURLを生成する
-    # @param [Nokogiri::XML::Element] elm
-    # @return [String]
-    def url_of_detail_contents(elm)
-      "#{TARGET_SITE}#{elm.css('a').first['href']}"
+    # 渡されたNokogiri::XML::Elementからアンカータグを検索し
+    # href属性から詳細ページへのリンクを生成する。
+    # @return [String, Nil] アンカータグがない場合はnil
+    def link_to_detail_page(elm)
+      a_tag = elm.css('a').first
+      a_tag.nil? ? nil : "#{TARGET_SITE}#{a_tag['href']}"
     end
 
     # 更新履歴のリンク情報から詳細ページの情報を取得する
     # @param [String] link 詳細ページのURL
-    # @return [String]
+    # @return [String, Nil] linkがnilの場合はnil
     def contents_of_detail_page(link)
+      return nil if link.nil?
       doc = Nokogiri::HTML(open(link))
       doc.css('.skip ~ *').map { |elm| elm.text }.join("\n")
+    end
+
+    # 前回処理時間から指定時間以内かつ、@resultが空でない場合はtrue
+    # @return [Boolean]
+    def use_result_of_previous?
+      within_specified_seconds? && !@result.empty?
+    end
+
+    # 前回実行時間から指定の秒数以内かどうかを返す
+    # @return [Boolean]
+    #   true  : 指定時間以内
+    #   false : 指定時間以上
+    def within_specified_seconds?
+      SPECIFIED_TIME > (Time.now - @last_update)
     end
   end
 end
